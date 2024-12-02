9c1555992489b806462615774bf48bccab5cd558
name <ff>
2024-12-02 17:40:48 +0800

    111

--- question_e.m
+++ question_e.m
@@ -11,7 +11,7 @@ n_en = pp + 1;         % number of element or local nodes
 n_el = 4;              % number of elements
 n_np = n_el * pp + 1;  % number of nodal points
 n_eq = n_np - 1;       % number of equations
-n_int = 10;
+n_int = i;
 
 hh = 1.0 / (n_np - 1); % space between two adjacent nodes
 x_coor = 0 : hh : 1;   % nodal coordinates for equally spaced nodes
@@ -125,41 +125,6 @@ for ee = 1 : n_el
 end
 
 
-plot(x_sam, u_sam, '-r*','LineWidth',1);
-hold on;
-plot(x_sam, y_sam, '-b','LineWidth',1);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-% EOF
+% plot(x_sam, u_sam, '-r*','LineWidth',1);
+% hold on;
+% plot(x_sam, y_sam, '-b','LineWidth',1);
